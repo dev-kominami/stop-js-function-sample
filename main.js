1e07fced80c3d5be3c359c6731140740f40f27bc
Takashi Kominami <dev.kominami@gmail.com>
2024-09-14 00:05:18 +0900

    refactor: 💡 main.js

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
+// グローバル変数としてworkerを宣言
 let worker;
 
+// Workerを作成し、メッセージハンドラを設定する関数
+function createWorker() {
+  worker = new Worker("worker.js");
+  worker.onmessage = function(event) {
+    // Workerからのメッセージを結果エリアに追加
+    document.getElementById("result").innerHTML += event.data + "<br>";
+  };
+  // Worker開始メッセージを表示
+  document.getElementById("result").innerHTML += "Worker が開始しました<br>";
+}
+
+// Workerを停止する関数
+function stopWorker() {
+  if (worker) {
+    worker.terminate(); // Workerを終了
+    worker = undefined; // worker変数をリセット
+    // Worker停止メッセージを表示
+    document.getElementById("result").innerHTML += "Worker が停止しました<br>";
+  }
+}
+
+// 開始ボタンのクリックイベントハンドラ
 document.getElementById('startWorker').addEventListener('click', () => {
   if (typeof(Worker) !== "undefined") {
-    if (!worker) {
-      worker = new Worker("worker.js");
-      worker.onmessage = function(event) {
-        document.getElementById("result").innerHTML += event.data + "<br>";
-      };
-    } else {
-      // 既存のWorkerが存在する場合は一度終了させて、新しいWorkerを作成
-      worker.terminate();
-      worker = new Worker("worker.js");
-      worker.onmessage = function(event) {
-        document.getElementById("result").innerHTML += event.data + "<br>";
-      };
+    // Workerがサポートされている場合
+    if (worker) {
+      // 既存のWorkerがある場合は停止
+      stopWorker();
     }
-    document.getElementById("result").innerHTML += "Worker が開始しました<br>";
+    // 新しいWorkerを作成
+    createWorker();
   } else {
+    // Workerがサポートされていない場合
     console.log("Web Workerはサポートされていません");
   }
 });
 
-document.getElementById('stopWorker').addEventListener('click', () => {
-  if (worker) {
-    worker.terminate();
-    worker = undefined;
-    document.getElementById("result").innerHTML += "Worker が停止しました<br>";
-  }
-});
+// 停止ボタンのクリックイベントハンドラ
+document.getElementById('stopWorker').addEventListener('click', stopWorker);
