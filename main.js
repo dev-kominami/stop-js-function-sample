599d8d967b9f206b47125b7be09713deaac04710
Takashi Kominami <dev.kominami@gmail.com>
2024-09-14 00:01:04 +0900

    feat: 🎸 停止後workerを再実行できるように修正

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ document.getElementById('startWorker').addEventListener('click', () => {
       worker.onmessage = function(event) {
         document.getElementById("result").innerHTML += event.data + "<br>";
       };
+    } else {
+      // 既存のWorkerが存在する場合は一度終了させて、新しいWorkerを作成
+      worker.terminate();
+      worker = new Worker("worker.js");
+      worker.onmessage = function(event) {
+        document.getElementById("result").innerHTML += event.data + "<br>";
+      };
     }
+    document.getElementById("result").innerHTML += "Worker が開始しました<br>";
   } else {
     console.log("Web Workerはサポートされていません");
   }
